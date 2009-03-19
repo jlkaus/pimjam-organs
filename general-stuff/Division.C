17742cfacafd0f689988a4fcf44a0b5cbb689940
Jonathan Kaus <jlkaus@gmail.com>
2009-03-19 23:12:03 +0000

    jlkaus: Roughed out the xml organ loading

--- general-stuff/Division.C
+++ general-stuff/Division.C
@@ -1,14 +1,41 @@
 #include "Division.H"
 #include "Input.H"
 #include "AlsaPlayControlBlock.H"
-
+#include <map>
+#include <ostream>
+#include "ticpp.h"
+#include "Rank.H"
+#include <set>
 
 Division::Division(ticpp::Element* divisionDescription) {
+	try {
+		mName = divisionDescription->GetAttribute("name");
+		std::cout << "\tBuilding Division of name: "<<mName << std::endl;
 
+		ticpp::Iterator<ticpp::Element> child("keyboard");
+		for(child = child.begin(divisionDescription); child!= child.end(); ++child) {
+			std::string childName = child->GetAttribute("name");
+			int childChannel = child->GetAttribute("channel");
+			int childOffset = child->GetAttribute("length",FALSE);
+			
+			std::cout << "\t\tAdding Keyboard of name "<<childName<<" with offset "<<childOffset<<" on channel "<<childChannel<<std::endl;
+			
+			mKeyboards[Input(childChannel)] = Keyboard(childName, childOffset);
+		}
+
+
+
+	} catch(ticpp::Exception & ex) {
+		std::cout << "*** Something horrible happened: "<< ex.What() << std::endl;
+	}
 }
 
 Division::~Division() {
-
+	std::map<Stop,Rank*>::iterator curRank;
+	for(curRank = mRanks.begin(); curRank != mRanks.end(); ++curRank) {
+		delete curRank.second;
+		mRanks.erase(curRank);
+	}
 }
 
 
--- general-stuff/Organ.C
+++ general-stuff/Organ.C
@@ -2,16 +2,36 @@
 #include <string>
 #include "Input.H"
 #include "AlsaPlayControlBlock.H"
+#include "ticpp.h"
+#include <ostream>
 
 Organ::Organ(std::string organFile) {
-  // open the organ file
-  // parse it as xml
-  // creating a Division for each division section of the file and passing that element in the constructor of the Division.
-  // close the organ file
+  try {
+	  ticpp::Document doc(organFile);
+	  doc.LoadFile();
+
+	  ticpp::Element* organElement = doc.FirstChildElement();
+
+	  std::string organName = organElement->GetAttribute("name");
+
+	  std::cout << "Reading Organ from "<<organFile<<" by name of "<< organName <<std::endl;
+	  ticpp::Iterator<ticpp::Element> child("division");
+	  for(child = child.begin(organElement); child!=child.end(); ++child) {
+		std::string divName = child->GetAttribute("name");
+		std::cout << "\tGoing to create a new division with a name of "<<divName<<std::endl;
+		mDivisions[divName] = new Division(child);
+	  }
+  } catch(ticpp::Exception& ex) {
+	std::cout << "*** Something bad happened.  Here is the exception: "<<ex.What() << std::endl;
+  }
 }
 
 Organ::~Organ() {
-  // delete all the Divisions
+	std::map<std::string, Division*>::iterator curDiv;
+	for(curDiv = mDivisions.begin(); curDiv != mDivisions.end(); ++curDiv) {
+		delete curDiv->second;
+		mDivisions.erase(curDiv);
+	}
 }
 
 int Organ::sendInput(const Input& in, AlsaPlayControlBlock& apcb) {
