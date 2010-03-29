d6b7661e56ce81189178ceb08f8c3ee66670bbd4
Jonathan Kaus <jlkaus@gmail.com>
2010-03-29 20:03:57 +0000

    jlkaus: rough version of new firmware

---
+++ firmware-stuff/config_structures.h
@@ -0,0 +1,120 @@
+#ifndef _CONFIG_STRUCTURES_H
+#define _CONFIG_STRUCTURES_H
+
+#include <inttypes.h>
+#include <stddefs.h>
+
+struct half_board_settings_t {
+	uint8_t address_mask;
+	uint8_t buffer_shift;
+	uint8_t channel_num;
+	uint8_t key_offset;
+};
+
+struct control_settings_t {
+	uint8_t admux;
+	uint8_t adc_shift;
+	uint8_t channel_num;
+	uint8_t control_num;
+};
+
+struct half_board_values_t {
+	uint8_t buffer_values[4];
+};
+
+struct control_values_t {
+	uint8_t control_value;
+	uint8_t rsvd[3];
+};
+
+#defined CONFIG_LAYOUT_VERSION 0x01
+struct config_t {
+  // basic settings (grouped into reasonable 32bit chunks)
+	uint16_t config_copy_size;		// 00-01
+	uint8_t config_layout_version;		// 02
+	uint8_t rsvd0;				// 03
+	
+	uint8_t rsvd1;				// 04
+	uint8_t firmware_date_string_length;	// 05
+	uint16_t firmware_date_string_offset;	// 06-07
+	
+	uint8_t rsvd2;				// 08
+	uint8_t firmware_boot_string_length;	// 09
+	uint16_t firmware_boot_string_offset;	// 0A-0B
+	
+	uint8_t firmware_version;		// 0C
+	uint8_t rsvd3;				// 0D
+	uint16_t sleep_time;			// 0E-0F
+
+	uint16_t uart_baud_rate;		// 10-11
+	uint8_t rsvd4;				// 12
+	uint8_t rsvd5;				// 13
+	
+	uint8_t rsvd6;				// 14
+	uint8_t num_half_boards;		// 15
+	uint16_t buffer_wait_time;		// 16-17
+	
+	uint8_t rsvd7;				// 18
+	uint8_t num_controls;			// 19
+	uint16_t adc_wait_time;			// 1A-1B
+	
+	uint8_t rsvd8[4];			// 1C-1F
+
+  // string area
+	char date_string[16];			// 20-2F
+	char boot_string[16];			// 30-3F
+	
+  // half-board and control settings arrays
+  	struct half_board_settings_t half_board_settings[8];	// 40-5F
+	struct control_settings_t control_settings[8];		// 60-7F
+
+  // half-board and control last value arrays
+  	struct half_board_values_t half_board_values[8];	// 80-9F
+	struct control_values_t control_values[8];		// A0-BF
+
+  // reserved space for future expansion
+  	char rsvd7[0x100-0xC0];					// C0-FF
+};
+
+#include config_defaults.h
+
+#define CONFIG_DEFAULT_INITIALIZER { \
+	sizeof(struct config_t), CONFIG_LAYOUT_VERSION, RSVD_VAL, \
+	RSVD_VAL, sizeof(FW_DATE), offsetof(struct config_t, date_string), \
+	RSVD_VAL, sizeof(FW_BOOT), offsetof(struct config_t, boot_string), \
+	FW_VERSION, RSVD_VAL, CFG_SLEEP_TIME, \
+	CFG_BAUD_RATE, RSVD_VAL, RSVD_VAL, \
+	RSVD_VAL, CFG_NUM_HALF_BOARDS, CFG_BUFFER_WAIT_TIME, \
+	RSVD_VAL, CFG_NUM_CONTROLS, CFG_ADC_WAIT_TIME, \
+	{RSVD_VAL}, \
+	FW_DATE, FW_BOOT, \
+	{ \
+		{ CFG_ADDR_MASK_H0, CFG_BUF_SHIFT_H0, CFG_CHAN_NUM_H0, CFG_KEY_OFFS_H0 }, \
+		{ CFG_ADDR_MASK_H1, CFG_BUF_SHIFT_H1, CFG_CHAN_NUM_H1, CFG_KEY_OFFS_H1 }, \
+		{ CFG_ADDR_MASK_H2, CFG_BUF_SHIFT_H2, CFG_CHAN_NUM_H2, CFG_KEY_OFFS_H2 }, \
+		{ CFG_ADDR_MASK_H3, CFG_BUF_SHIFT_H3, CFG_CHAN_NUM_H3, CFG_KEY_OFFS_H3 }, \
+		{ CFG_ADDR_MASK_H4, CFG_BUF_SHIFT_H4, CFG_CHAN_NUM_H4, CFG_KEY_OFFS_H4 }, \
+		{ CFG_ADDR_MASK_H5, CFG_BUF_SHIFT_H5, CFG_CHAN_NUM_H5, CFG_KEY_OFFS_H5 }, \
+		{ CFG_ADDR_MASK_H6, CFG_BUF_SHIFT_H6, CFG_CHAN_NUM_H6, CFG_KEY_OFFS_H6 }, \
+		{ CFG_ADDR_MASK_H7, CFG_BUF_SHIFT_H7, CFG_CHAN_NUM_H7, CFG_KEY_OFFS_H7 } \
+	}, { \
+		{ CFG_ADMUX_C0, CFG_ADC_SHIFT_C0, CFG_CHAN_NUM_C0, CFG_CNTRL_NUM_C0 }, \
+		{ CFG_ADMUX_C1, CFG_ADC_SHIFT_C1, CFG_CHAN_NUM_C1, CFG_CNTRL_NUM_C1 }, \
+		{ CFG_ADMUX_C2, CFG_ADC_SHIFT_C2, CFG_CHAN_NUM_C2, CFG_CNTRL_NUM_C2 }, \
+		{ CFG_ADMUX_C3, CFG_ADC_SHIFT_C3, CFG_CHAN_NUM_C3, CFG_CNTRL_NUM_C3 }, \
+		{ CFG_ADMUX_C4, CFG_ADC_SHIFT_C4, CFG_CHAN_NUM_C4, CFG_CNTRL_NUM_C4 }, \
+		{ CFG_ADMUX_C5, CFG_ADC_SHIFT_C5, CFG_CHAN_NUM_C5, CFG_CNTRL_NUM_C5 }, \
+		{ CFG_ADMUX_C6, CFG_ADC_SHIFT_C6, CFG_CHAN_NUM_C6, CFG_CNTRL_NUM_C6 }, \
+		{ CFG_ADMUX_C7, CFG_ADC_SHIFT_C7, CFG_CHAN_NUM_C7, CFG_CNTRL_NUM_C7 } \
+	}, \
+	{UNSET_VAL}, {UNSET_VAL}, \
+	{UNSET_VAL} }
+
+
+	
+
+
+
+
+#endif
+
---
+++ firmware-stuff/organbop.c
@@ -0,0 +1,201 @@
+#include <inttypes.h>
+#include <avr/io.h>
+#include <avr/pgmspace.h>
+#include <avr/interrupt.h>
+#include <avr/wdt.h>
+#include <avr/boot.h>
+#include <util/delay.h>
+#include <avr/eeprom.h>
+
+#define FW_DATE "2010-04-01"
+#define FW_BOOT "OrganBop BOOTED"
+#define FW_VERSION 0x11
+
+#include "config_structures.h"
+
+
+/* SW_MAJOR and MINOR needs to be updated from time to time to avoid warning message from AVR Studio */
+/* never allow AVR Studio to do an update !!!! */
+#define HW_VER	 0x02
+#define SW_MAJOR 0x01
+#define SW_MINOR 0x10
+
+/* onboard LED is used to indicate, that the bootloader was entered (3x flashing) */
+/* if monitor functions are included, LED goes on after monitor was entered */
+#define LED_DDR  DDRB
+#define LED_PORT PORTB
+#define LED_PIN  PINB
+#define LED      PINB0
+
+#define NUM_LED_FLASHES 3
+#define MAX_TIME_COUNT 8000000L>>1
+
+#define BUFFERS_PER_HALF_BOARD 4
+
+/* define various device id's */
+/* manufacturer byte is always the same */
+#define SIG1	0x1E	// Yep, Atmel is the only manufacturer of AVR micros.  Single source :(
+#define SIG2	0x96
+#define SIG3	0x09
+#define PAGE_SIZE		0x080U   //128 words
+#define PAGE_SIZE_BYTES	0x100U   //256 bytes
+
+struct config_t liveCopy;
+struct config_t savedCopy __attributes__ ((section (".eeprom"))) = CONFIG_DEFAULT_INITIALIZER;
+struct config_t backupCopy __attributes__ ((section (".eeprom"))) = CONFIG_DEFAULT_INITIALIZER;
+
+
+/* function prototypes */
+void putch(char);
+char getch(void);
+char getch_async(void);
+void flash_led(uint8_t count);
+
+/* main program starts here */
+int main(void)
+{
+    //uint8_t ch;
+    uint8_t cur_state[NUM_INPUT_BUFFERS];
+    uint8_t i,j;
+	
+    asm volatile("nop\n\t");
+
+    //initialize our serial port.
+    UBRR0L = (uint8_t)(F_CPU/(BAUD_RATE*16L)-1);
+    UBRR0H = (F_CPU/(BAUD_RATE*16L)-1) >> 8;
+    UCSR0B = (1<<RXEN0) | (1<<TXEN0);
+    UCSR0C = (1<<UCSZ00) | (1<<UCSZ01);
+
+    /* Enable internal pull-up resistor on pin D0 (RX), in order
+    to supress line noise */
+    DDRD &= ~_BV(PIND0);
+    PORTD |= _BV(PIND0);
+
+    /* Set data direction of PORTC for input */
+    DDRC &= 0x00;
+    PORTC |= 0xFF;
+
+    /* set LED pin as output */
+    LED_DDR |= _BV(LED);
+
+    /* set data direction of PORTB for output */
+    DDRB |= 0xFF;
+    PORTB &= 0x00;
+
+    ADCSRB = 0x00; /* Select free running mode */
+    DIDR0 = 0xFF;  /* Disable digital inputs on analog pins */
+    ADCSRA = 0xE7; /* 0xE0 - Enables & starts A/D Converter */
+                   /* 0x07 - Sets frequency prescaler to 128 */
+
+// LOAD liveCopy from eepromCopy
+
+
+    /* Initialize the A/D converter */
+    ADMUX = 0xC7;  /* 0x40 sets reference voltage to AVCC (0xC0 sets reference voltage to 2.56V) */
+   
+// Display boot message
+// flash LEDs 3 times, then turn off
+    
+    flash_led(NUM_LED_FLASHES);
+    putch(0xAF);
+    putch(0xAE);
+    putch(0x07);
+    putch('O');
+    putch('B');
+    putch('F');
+    putch('W');
+    putch('v');
+    putch('1');
+    putch('1');
+    putch(0xAD);
+    putch(0xAC);
+
+    /* forever loop */
+    for (;;)
+    {
+	getch();
+	putch(ADCSRA);
+	putch(ADCL);
+	putch(ADCH);
+
+	PORTB = getch();
+	_delay_us(5);
+	putch(PINC);
+	continue;
+
+	for(i = 0; i < NUM_INPUT_BUFFERS; ++i) {
+	    uint8_t addr_tmp = (i | ((~i & 0x18) << 2)) << 1;
+
+	    PORTB = addr_tmp;
+	    uint8_t tmp = cur_state[i];
+	    _delay_us(5);
+	    cur_state[i] = PINC;
+	    tmp = tmp ^ cur_state[i];
+	    for(j = 0; j < 8; ++j) {
+    		if(tmp & (1 << j)) {
+		    uint8_t state_val = cur_state[i] & (1 << j) ? 1 : 0;
+
+                    /* flash_led(1); */
+
+                    /* Send changed event */
+		    putch(0xFF);
+		    putch(0xFE);
+		    putch(state_val);
+                    putch(i);
+                    putch(j);
+		    putch(0);
+                    putch(0xFD);
+		    putch(0xFC);
+		}
+	    }
+	}
+
+	/* Wait 20 ms before polling again */
+	_delay_ms(20);
+    }
+    /* end of forever loop */
+}
+
+
+void putch(char ch)
+{
+    while (!(UCSR0A & _BV(UDRE0)));
+    UDR0 = ch;
+}
+
+
+char getch(void)
+{
+    while(!(UCSR0A & _BV(RXC0)));
+    return UDR0;
+}
+
+char getch_async(void)
+{
+  if(UCSR0A & _BV(RXC0)) {
+    return UDR0;
+  } else {
+    return 0x00;
+  }
+}
+
+void flash_led(uint8_t count)
+{
+    /* flash onboard LED three times to signal entering of bootloader */
+	/* l needs to be volatile or the delay loops below might get
+	optimized away if compiling with optimizations (DAM). */
+
+    if (count == 0)
+    {
+      count = 3;
+    }
+
+	int8_t i;
+    for (i = 0; i < count; ++i)
+    {
+	LED_PORT |= _BV(LED);
+	_delay_ms(100);
+	LED_PORT &= ~_BV(LED);
+	_delay_ms(100);
+    }
+}
